0dc32bee88a567b8be3779402ac164de9ddcf05c
JuanAguirre10 <juancho71096@gmail.com>
2026-06-27 14:22:09 -0500

    fix3

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Middlewares
 app.use(cors({
-  origin: 'https://frontend-marketplace-ashen.vercel.app/',
+  origin: 'https://frontend-marketplace-ashen.vercel.app',
   credentials: true
 }));
 app.use(express.json());
